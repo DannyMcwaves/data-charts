02d9c94834ade346469f84c5909a57ce6e50212e
dannymcwaves <dannymcwaves96@gmail.com>
2020-01-15 17:58:56 +0300

    electron window config values and static data

---
+++ src/main/config.ts
@@ -0,0 +1,15 @@
+export const windowDefaults = {
+    width: 1100,
+    height: 700,
+    minWidth: 650,
+    useContentSize: true,
+    title: 'Chartly',
+    center: true,
+    show: true,
+    resizable: false,
+    movable: true,
+    maximizable: true,
+    webPreferences: {
+        webSecurity: true // TODO: Remove in production!
+    }
+};
