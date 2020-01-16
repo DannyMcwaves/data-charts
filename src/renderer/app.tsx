2a5094ee0bb8dbe623f516621ecb44d2b3d456d5
dannymcwaves <dannymcwaves96@gmail.com>
2020-01-16 19:16:21 +0300

    renderer app updates

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -3,11 +3,16 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
 
-import Application from './components/Application';
+import Application from './components/app/Application';
 import store from './store';
+
+import './app.scss';
+import 'bootstrap-4-grid/scss/grid.scss';
 
 // Create main element
 const mainElement = document.createElement('div');
+mainElement.id = 'main';
+mainElement.className = 'container-fluid';
 document.body.appendChild(mainElement);
 
 // Render components
