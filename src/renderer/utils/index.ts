d1aa484c1fcb542d77e492736b02692bb977cf21
dannymcwaves <dannymcwaves96@gmail.com>
2020-01-19 18:16:45 +0300

    general purpose utils

---
+++ src/renderer/utils/index.ts
@@ -0,0 +1,4 @@
+
+// an array containing data of any type and we randomly pick one data from the array
+export const RandomItemInArray = (array: any[]) => array[Math.floor(Math.random()*array.length)];
+
